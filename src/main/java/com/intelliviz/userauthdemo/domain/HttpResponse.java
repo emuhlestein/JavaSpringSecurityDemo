bef1556d6583e08d5f023b5cd2b257b0a582082a
emuhlestein <emuhlestein@yahoo.com>
2020-09-11 16:02:29 -0500

    Fix time/date format

--- src/main/java/com/intelliviz/userauthdemo/domain/HttpResponse.java
+++ src/main/java/com/intelliviz/userauthdemo/domain/HttpResponse.java
@@ -1,8 +1,20 @@
 package com.intelliviz.userauthdemo.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import org.springframework.http.HttpStatus;
 
+import java.time.LocalDateTime;
+
 public class HttpResponse {
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    private LocalDateTime timeStamp;
+
     private int httpStatusCode;
     private HttpStatus httpStatus;
     private String reason;
@@ -12,10 +24,19 @@ public class HttpResponse {
     }
 
     public HttpResponse(int httpStatusCode, HttpStatus httpStatus, String reason, String message) {
+        this.timeStamp = LocalDateTime.now();
         this.httpStatusCode = httpStatusCode;
         this.httpStatus = httpStatus;
         this.reason = reason;
         this.message = message;
+    }
+
+    public LocalDateTime getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(LocalDateTime timeStamp) {
+        this.timeStamp = timeStamp;
     }
 
     public int getHttpStatusCode() {
